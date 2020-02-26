9cff616f8febc459cf0b37e365e58b13544122e2
Arman Yaraee <arman@yaraee.net>
2020-02-26 19:33:59 +0000

    Removed unused files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
 import Home from './containers/Home';
 
 class App extends React.Component {
