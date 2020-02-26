cac3463cf024ae080e039f93613af99c7d50c9c4
Arman Yaraee <arman@yaraee.net>
2020-02-26 16:16:38 +0000

    Adding base client

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Home from './containers/Home';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
