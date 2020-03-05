bb95d96cfad7e4ff921d7a246ca82451bd17c0b7
Arman Yaraee <arman@yaraee.net>
2020-03-05 07:48:21 +0000

    Move base url to root

--- client/src/__tests__/tools/clsx.test.ts
+++ client/src/__tests__/tools/clsx.test.ts
@@ -1,4 +1,4 @@
-import clsx from 'tools/clsx';
+import clsx from 'src/tools/clsx';
 
 it("generates the classnames", () => {
   const result = clsx("a", "z" && false, {"b": false, "c": undefined, "d": null, "e": true});
